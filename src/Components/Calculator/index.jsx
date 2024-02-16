87628ca2c598c20ea91182615b5af6f9444b47e9
sergioandresco <sergiocobos6@outlook.com>
2024-02-15 21:23:31 -0500

    creation a initial structure and differents components about the calculator

---
+++ src/Components/Calculator/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Calculator.css'
+import { Display } from '../Display'
+
+function Calculator(){
+    return(
+        
+        <React.Fragment>
+
+            <Display />
+
+        </React.Fragment>
+
+    );
+}
+
+export { Calculator };
---
+++ src/Components/Display/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Display.css'
+
+function Display(){
+    return(
+
+        <textarea rows="4" cols="50">
+            Enter the numbers
+        </textarea>
+
+    );
+}
+
+export { Display }
